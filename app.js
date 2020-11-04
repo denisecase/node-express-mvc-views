c5b4ad0d7b29f3f1def7b3cb68191bc23798cbae
Denise Case <denisecase@gmail.com>
2020-11-04 13:42:14 -0600

    initial update F20 (app starts - more work on controllers etc needed)

--- app.js
+++ app.js
@@ -6,55 +6,97 @@
  *
  * The server-side app starts and begins listening for events.
  *
- *  @requires config
  *  @requires express
- **/
-const config = require('config')
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
-const helmet = require('helmet')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const engines = require('consolidate')
-const expressLayouts = require('express-ejs-layouts')
-const app = express() // create an Express web app
+ * */
+const dotenv = require('dotenv');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const engines = require('consolidate');
+const expressLayouts = require('express-ejs-layouts');
 
-// Use hosting values if available, otherwise default
-const environment = process.env.NODE_ENV || 'development'
-const hostname = process.env.HOSTNAME || config.get('hostname')
-const port = process.env.PORT || config.get('port')
+const LOG = require('./utils/logger');
+
+// env variables
+const hostname = process.env.HOSTNAME;
+const isProduction = process.env.NODE_ENV === 'production';
+LOG.info('Environment isProduction = ', isProduction);
+
+// create an Express app
+const app = express();
+LOG.info('app created');
+
+// Helper functions defined first ...................................
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+};
+
+/**
+ * Load environment variables from .env file,
+ *  where API keys and passwords can be configured.
+ */
+const vars = dotenv.config({ path: '.env' });
+if (vars.error) {
+  throw vars.error;
+}
+LOG.info(`Environment variables loaded: ${vars.parsed}`);
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(process.env.PORT);
+app.set('port', port);
+LOG.info(`Server Launch at port: ${port}`);
 
 // By default, Express does not serve static files.
 // use middleware to define a static assets folder 'public'
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Helmet helps you secure Express apps by setting various HTTP headers.
 // It's not a silver bullet, but it can help!
 // https://github.com/helmetjs/helmet
-app.use(helmet())
+app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.engine('ejs', engines.ejs)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', engines.ejs);
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(expressLayouts)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressLayouts);
 
 // load seed data
-require('./utils/seeder.js')(app)
+require('./utils/seeder.js')(app);
 
 // Use Express middleware to configure routing
-const routing = require('./routes/router.js')
-app.use('/', routing)
+const routing = require('./routes/router.js');
+
+app.use('/', routing);
 
 app.listen(port, hostname, () => {
-  console.log(`App running at http://${hostname}:${port}/ in ${environment}`)
-  console.log('Hit CTRL-C CTRL-C to stop\n')
-})
+  console.log(
+    `App running at http://${hostname}:${port}/ in ${process.env.NODE_ENV}`,
+  );
+  console.log('Hit CTRL-C CTRL-C to stop\n');
+});
--- routes/router.js
+++ routes/router.js
@@ -7,34 +7,35 @@
  * @requires express
  */
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+
+const router = express.Router();
 
 // Manage top-level request first
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   // res.sendFile('index.html')
-  res.render('index', { title: 'MVC' })
-})
+  res.render('index', { title: 'MVC' });
+});
 
-router.get('/index', (req, res, next) => {
-  res.render('index', { title: 'MVC' })
-})
+router.get('/index', (req, res) => {
+  res.render('index', { title: 'MVC' });
+});
 
 // Route requests that start with '/dev' to a particular controller
-router.use('/developer', require('../controllers/developer.js'))
+router.use('/developer', require('../controllers/developer.js'));
 
 // catch 404 and forward to error handler
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
 
 // error handler
-router.use((err, req, res, next) => {
+router.use((err, req, res) => {
   // render the error page
-  res.status(err.status || 500)
-  res.render('error', { status: err.status, message: err.message })
-})
+  res.status(err.status || 500);
+  res.render('error', { status: err.status, message: err.message });
+});
 
-module.exports = router
+module.exports = router;
